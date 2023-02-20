0a017f77fa679abfb474a6a853dfdbf6002a9bfa
arjun.sridhar12345 <arjun.sridhar1998@gmail.com>
2023-02-20 14:00:21 -0800

    try and use executable to generate images

--- Software/qc_check.py
+++ Software/qc_check.py
@@ -34,6 +34,75 @@ class qcChecker():
         self.kpIdx = np.where(~np.isnan(self.spikeDepths[self.spikeIdx]) &
                                ~np.isnan(self.spikeAmps[self.spikeIdx]))[0]
     
+    # helper function for firing rate 
+    def bincount2D(x, y, xbin=0, ybin=0, xlim=None, ylim=None, weights=None):
+        """
+        Computes a 2D histogram by aggregating values in a 2D array.
+
+        :param x: values to bin along the 2nd dimension (c-contiguous)
+        :param y: values to bin along the 1st dimension
+        :param xbin:
+            scalar: bin size along 2nd dimension
+            0: aggregate according to unique values
+            array: aggregate according to exact values (count reduce operation)
+        :param ybin:
+            scalar: bin size along 1st dimension
+            0: aggregate according to unique values
+            array: aggregate according to exact values (count reduce operation)
+        :param xlim: (optional) 2 values (array or list) that restrict range along 2nd dimension
+        :param ylim: (optional) 2 values (array or list) that restrict range along 1st dimension
+        :param weights: (optional) defaults to None, weights to apply to each value for aggregation
+        :return: 3 numpy arrays MAP [ny,nx] image, xscale [nx], yscale [ny]
+        """
+        # if no bounds provided, use min/max of vectors
+        if xlim is None:
+            xlim = [np.min(x), np.max(x)]
+        if ylim is None:
+            ylim = [np.min(y), np.max(y)]
+
+        def _get_scale_and_indices(v, bin, lim):
+            # if bin is a nonzero scalar, this is a bin size: create scale and indices
+            if np.isscalar(bin) and bin != 0:
+                scale = np.arange(lim[0], lim[1] + bin / 2, bin)
+                ind = (np.floor((v - lim[0]) / bin)).astype(np.int64)
+            # if bin == 0, aggregate over unique values
+            else:
+                scale, ind = np.unique(v, return_inverse=True)
+            return scale, ind
+
+        xscale, xind = _get_scale_and_indices(x, xbin, xlim)
+        yscale, yind = _get_scale_and_indices(y, ybin, ylim)
+        # aggregate by using bincount on absolute indices for a 2d array
+        nx, ny = [xscale.size, yscale.size]
+        ind2d = np.ravel_multi_index(np.c_[yind, xind].transpose(), dims=(ny, nx))
+        r = np.bincount(ind2d, minlength=nx * ny, weights=weights).reshape(ny, nx)
+
+        # if a set of specific values is requested output an array matching the scale dimensions
+        if not np.isscalar(xbin) and xbin.size > 1:
+            _, iout, ir = np.intersect1d(xbin, xscale, return_indices=True)
+            _r = r.copy()
+            r = np.zeros((ny, xbin.size))
+            r[:, iout] = _r[:, ir]
+            xscale = xbin
+
+        if not np.isscalar(ybin) and ybin.size > 1:
+            _, iout, ir = np.intersect1d(ybin, yscale, return_indices=True)
+            _r = r.copy()
+            r = np.zeros((ybin.size, r.shape[1]))
+            r[iout, :] = _r[ir, :]
+            yscale = ybin
+
+        return r, xscale, yscale
+
+    def get_fr(self):
+        T_BIN = 0.05
+        D_BIN = 5
+        chn_min = np.min(np.r_[self.chn_min, self.spikes['depths'][self.spike_idx][self.kp_idx]])
+        chn_max = np.max(np.r_[self.chn_max, self.spikes['depths'][self.spike_idx][self.kp_idx]])
+        n, times, depths = self.bincount2D(self.spikes['times'][self.spike_idx][self.kp_idx],
+                                          self.spikes['depths'][self.spike_idx][self.kp_idx],
+                                          T_BIN, D_BIN, ylim=[chn_min, chn_max])
+
     # gets the amplitude
     def get_amp(self):
         A_BIN = 10
